d6d0699e4f3ae9eea82353a0909a74930b256365
Aleksandr Shatskih <work@errand.ru>
2023-03-11 22:11:06 +0700

    refactor: two video fields instead of one

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import './style.css'
 import { fetchJSON } from "./tools/fetcher";
-import {openModal} from "./tools/openModal";
-import {updateURL} from "./tools/updateURL";
+import { openModal } from "./tools/openModal";
+import { updateURL } from "./tools/updateURL";
 
 const videoLinks = document.querySelectorAll('[data-video]');
+const yt_blocked = document.body.classList.contains('yt-blocked');
 
 if(videoLinks) {
     Array.from(videoLinks).forEach((link) => {
@@ -14,7 +15,14 @@ if(videoLinks) {
                 const video_id = target.dataset.video;
                 if(video_id) {
                     fetchJSON(`/wp-json/wp/v2/video/${video_id}`).then(data => {
-                        updateURL(data[0].acf.video_id);
+                        console.log(data)
+                        let source = 'youtube';
+                        if(!yt_blocked && data[0].acf.video_id.rutube !== 'null') {
+                            source = 'rutube';
+                        } else if (!yt_blocked && data[0].acf.video_id.youtube !== 'null') {
+                            source = 'youtube';
+                        }
+                        updateURL(data.acf.video_id[source]);
                         openModal(data)
                     })
                 }
@@ -29,8 +37,12 @@ if (url.includes('?video_id=')) {
     if(url.split('?video_id=')[1]) {
         const video_id = url.split('?video_id=')[1];
         fetchJSON(`/wp-json/wp/v2/video?meta_key=video_id&meta_value=${video_id}`).then(data => {
-            updateURL(data[0].acf.video_id);
-            openModal(data)
+            let source = 'youtube';
+            if(!yt_blocked || data[0].acf.video_id.rutube !== 'null') {
+                source = 'rutube';
+            }
+            updateURL(data[0].acf.video_id[source]);
+            openModal(data[0])
         });
     }
 }
